fa1ed34f7b76ba5ec45eec8ca6539aead068668d
Simone <tritto.simone@gmail.com>
2023-05-26 10:38:18 +0200

    implement_cards

--- js/script.js
+++ js/script.js
@@ -1,51 +1,109 @@
-// const card = document.querySelector('card');
-
+const cards = document.getElementById('users-publications');
+const heart = document.querySelector('.i-like');
 const arrayCards = [
     {
         phProfile : 'https://picsum.photos/60',
-        name : 'Ciccio Pasticcio',
+        name : 'Ciccio',
         date : '25/07/2022',
         picture : 'https://picsum.photos/900/300',
         like : 90, 
         comment: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis beatae praesentium omnis non obcaecati incidunt sapiente commodi. Iure excepturi vitae, in quas cum officia sit sint, itaque necessitatibus enim fuga.'
-    }
+    },
+    {
+        phProfile: 'https://picsum.photos/60',
+        name: 'Pasticcio',
+        date: '25/07/2022',
+        picture: 'https://picsum.photos/900/300',
+        like: 90,
+        comment: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis beatae praesentium omnis non obcaecati incidunt sapiente commodi. Iure excepturi vitae, in quas cum officia sit sint, itaque necessitatibus enim fuga.'
+    },
+    {
+        phProfile: 'https://picsum.photos/60',
+        name: 'Magna',
+        date: '25/07/2022',
+        picture: 'https://picsum.photos/900/300',
+        like: 90,
+        comment: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis beatae praesentium omnis non obcaecati incidunt sapiente commodi. Iure excepturi vitae, in quas cum officia sit sint, itaque necessitatibus enim fuga.'
+    },
+    {
+        phProfile: 'https://picsum.photos/60',
+        name: 'L\'arriccio',
+        date: '25/07/2022',
+        picture: 'https://picsum.photos/900/300',
+        like: 90,
+        comment: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis beatae praesentium omnis non obcaecati incidunt sapiente commodi. Iure excepturi vitae, in quas cum officia sit sint, itaque necessitatibus enim fuga.'
+    },
+    {
+        phProfile: 'https://picsum.photos/60',
+        name: 'Capriccio',
+        date: '25/07/2022',
+        picture: 'https://picsum.photos/900/300',
+        like: 90,
+        comment: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis beatae praesentium omnis non obcaecati incidunt sapiente commodi. Iure excepturi vitae, in quas cum officia sit sint, itaque necessitatibus enim fuga.'
+    },
+    {
+        phProfile: 'https://picsum.photos/60',
+        name: 'Laccio',
+        date: '25/07/2022',
+        picture: 'https://picsum.photos/900/300',
+        like: 90,
+        comment: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis beatae praesentium omnis non obcaecati incidunt sapiente commodi. Iure excepturi vitae, in quas cum officia sit sint, itaque necessitatibus enim fuga.'
+    },
+    {
+        phProfile: 'https://picsum.photos/60',
+        name: 'Taccio',
+        date: '25/07/2022',
+        picture: 'https://picsum.photos/900/300',
+        like: 90,
+        comment: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis beatae praesentium omnis non obcaecati incidunt sapiente commodi. Iure excepturi vitae, in quas cum officia sit sint, itaque necessitatibus enim fuga.'
+    },
 ];
 
-for (let i = 0; i < arrayCards.length; i++) {
-    let generateCard = `
-        <section class="container-top">
-            <div class="img-account">
-                <img src="${arrayCards[i].phProfile}" alt="">
-            </div>
+generateCard(arrayCards);
+heart.addEventListener('click', function(){
+    
+});
 
-            <div class="info-account">
-                <h2 class="name-account">
-                    ${arrayCards[i].name}
-                </h2>
 
-                <h5 class="date-ph">
-                    ${arrayCards[i].date}
-                </h5>
-            </div>
+function generateCard(array) {
+    for (let i = 0; i < array.length; i++) {
+        let generateCard = `
+        <section class="card">
+            <section class="container-top">
+                <div class="img-account">
+                    <img src="${array[i].phProfile}" alt="">
+                </div>
+
+                <div class="info-account">
+                    <h2 class="name-account">
+                        ${array[i].name}
+                    </h2>
+
+                    <h5 class="date-ph">
+                        ${array[i].date}
+                    </h5>
+                </div>
+            </section>
+
+            <section class="container-center">
+                <div class="ph">
+                    <img src="${array[i].picture}" alt="">
+                </div>
+
+                <div class="statistics">
+                    <button class="i-like">&#10084;</button>
+                    <h4 class="n-like">Piace a <strong>${array[i].like}</strong> persone</h4>
+                    <input type="text" class="write-comment" placeholder="scrivi un commento">
+                    <button class="send-comment">&#9997;</button>
+                </div>
+            </section>
+
+            <section class="container-bottom">
+                <p class="comment">${array[i].comment}</p>
+            </section>
         </section>
-
-        <section class="container-center">
-            <div class="ph">
-                <img src="${arrayCards[i].picture}" alt="">
-            </div>
-
-            <div class="statistics">
-                <button class="i-like">&#10084;</button>
-                <h4 class="n-like">Piace a <strong>${arrayCards[i].like}</strong> persone</h4>
-                <input type="text" class="write-comment" placeholder="scrivi un commento">
-                <button class="send-comment">&#9997;</button>
-            </div>
-        </section>
-
-        <section class="container-bottom">
-            <p class="comment">${arrayCards[i].comment}</p>
-        </section>
-
     `
-    document.querySelector('card').innerHTML = generateCard;
+        cards.innerHTML += generateCard;
+    }
 }
+
